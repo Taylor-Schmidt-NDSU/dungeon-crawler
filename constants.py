d6c3ddc4c365d5074844665cd8478e6037c22ad4
Taylor Schmidt <taylor.n.schmidt@ndus.edu>
2018-05-02 13:57:12 -0500

    Added constants module and keystroke manager
    Constants to store constants (duh)
    keystroke manager to separate where keyboard events are handled instead
    of jumbling it up into the level

---
+++ constants.py
@@ -0,0 +1,13 @@
+'''
+Created on May 2, 2018
+
+@author: taylor.n.schmidt
+'''
+# Define some colors
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+RED   = (255,   0,   0)
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+TILE_HEIGHT = 100
+TILE_WIDTH = 100
---
+++ keystroke_manager.py
@@ -0,0 +1,17 @@
+'''
+Created on May 2, 2018
+
+@author: taylor.n.schmidt
+'''
+
+class KeystrokeManager(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
