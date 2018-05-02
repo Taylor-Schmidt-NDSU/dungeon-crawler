a711f0973daf71842d5392f515f89e0d3e0c5325
Taylor Schmidt <taylor.n.schmidt@ndus.edu>
2018-05-02 13:52:46 -0500

    Added keystroke management delegation
    On init now creates a keystroke manager
    Added method handle_keyboard_event that passes event of to the keystroke
    manager

--- level_manager.py
+++ level_manager.py
@@ -5,6 +5,8 @@ Created on Apr 12, 2018
 '''
 from level import Level
 from player import Player
+from keystroke_manager import KeystrokeManager
+
 class LevelManager():
     '''
     classdocs
@@ -15,6 +17,7 @@ class LevelManager():
     class __LevelManager():
         def __init__(self, players):
             self.level = self.load_level(1, players)
+            self.keystroke_manager = KeystrokeManager()
             
         def load_level(self, level_number, players):
                 self.level = Level(level_number, players)
@@ -28,6 +31,9 @@ class LevelManager():
         
         def advance_level(self):
             self.load_level(self.level.level_number, self.leave_level())
+            
+        def handle_keyboard_event(self, event):
+            self.keystroke_manager.handle_keyboard_event(event)
 
     def __getattr__(self, name):
         return getattr(self.instance, name)
@@ -35,3 +41,5 @@ class LevelManager():
     def __init__(self):
         if not LevelManager.instance:
             LevelManager.instance = LevelManager.__LevelManager(Player())
+    
+    
