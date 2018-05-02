4e51911c47f5476f73276ea94bbd8138cbbb962b
Taylor Schmidt <taylor.n.schmidt@ndus.edu>
2018-05-02 13:55:55 -0500

    Updated main loop
    Need to implement delegation methods in level_manager. Shortened main
    method for clarity and readability

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ Created on Apr 30, 2018
 @author: taylor.n.schmidt
 '''
 import pygame
+import constants
 from level_manager import LevelManager
-# Initialize Pygame
 
+# Initialize Pygame
 pygame.init()
 pygame.mixer.init()
 
@@ -22,3 +23,17 @@ done = False
 
 # -------- Main Program Loop -----------
 while not done: 
+    
+    
+    level_manager.draw_level()
+    level_manager.update_level()
+    
+    clock.tick(60)
+    
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            done = True
+            break
+        
+        level_manager.handle_keyboard_event(event)
