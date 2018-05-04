5c186222d9235d2830d592cca48a7f80c4b04a15
Taylor Schmidt <taylor.n.schmidt@ndus.edu>
2018-05-03 19:42:00 -0500

    Added movement flags
    Implemented movement flags to be used in the update method to determine
    whether or not to move

--- player.py
+++ player.py
@@ -9,12 +9,19 @@ class Player(Character):
     '''
     classdocs
     '''
+    
     max_health = 100
 
     def __init__(self, params):
         '''
         Constructor
         '''
+        
         self.super.__init__(self.max_health)
         
+        #Set movement flags all to false
+        self.move_up_flag = False
+        self.move_down_flag = False
+        self.move_right_flag = False
+        self.move_left_flag = False
     
